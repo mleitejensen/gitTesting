a67d74b355e60c59e52dfd22f368328bd60dcf94
Martin <marekatt2004@gmail.com>
2023-11-09 10:02:38 +0100

    added javascript

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 let h1 = document.querySelector("h1")
 let h2 = document.querySelector("h2")
 
-h1.innerText = "Her er h1"
-h2.innerText = "Her er h2"
+console.log(h1.innerText)
+console.log(h2.innerText)
